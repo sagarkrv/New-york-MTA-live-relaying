e4e24d19af71f0e95ac126b25c5713e6032eae93
root <root@ip-172-31-88-226.ec2.internal>
2017-12-15 00:21:15 +0000

    all

--- exercise_1/transforming/readmissions.sql
+++
@@ -1,6 +0,0 @@
-SET hive.cli.print.header=true;
-DROP TABLE IF EXISTS rea_deat_parquet;
-CREATE TABLE rea_deat_parquet AS SELECT * FROM readmissions_and_deaths WHERE provider_id NOT LIKE 'Prov%';
-CACHE TABLE rea_deat_parquet;
-SELECT COUNT(*) readmissions_count FROM rea_deat_parquet;
-EXIT;
